bbaa4c5899127206f42584e692ff56b3b2b48e53
alireza <alireza.tadbir@devinci.fr>
2015-11-17 15:55:12 +0100

    STEP 6

--- lib/app.jsx
+++ lib/app.jsx
@@ -12,34 +12,21 @@ var timerValue=300;
 
 var Box = React.createClass({
 
-'componentWillMount':function onComponentWillMount(){
-        var self=this;
-        this.timer=setInterval(function onTimer(){
-            self.setState({
-            'value': self.state.value==='X' ? 'O' : 'X'
-        });
-        },timerValue );
-    },
-    /*componentWillMount: function(){
-        var old_this = this;
-        this.timer = setInterval(function(){
-        var oldValue = old_this.state.value;
+    handleClick: function(){
+        var oldValue = this.state.value;
         var newValue = oldValue === 'X' ? 'O': 'X';
-        old_this.setState({
+        this.setState({
         value: newValue
         });
-        }, timerValue);
-    },*/
-    componentWillUnmount: function(){
-        clearInterval(this.timer);
     },
+
     getInitialState: function() {
         return {value: this.props.initialValue};
     },
     'render': function onRender () {
     return (
         <div>
-            <button style={boxStyle}>{this.state.value}</button>
+            <button style={boxStyle} onClick={this.handleClick}>{this.state.value}</button>
         </div>
     );
    }
